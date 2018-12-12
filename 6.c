999f4c12e9998191c3a354ffb588b214eea89919
Brunda <lbrundaraju@gmail.com>
2018-12-13 01:22:48 +0530

    Update 6.c

--- 6.c
+++ 6.c
@@ -1,62 +1,26 @@
-#include <stdio.h> 
-#include <omp.h> 
+#include<omp.h>
+#include<stdio.h>
+#include<limits.h>
 #include<stdlib.h>
-#define MAXIMUM 65536
-
-void main() 
-{ 
-	int *array, i, Noofelements, cur_max, current_value; 
-	printf("Enter the number of elements\n");
-	scanf("%d", &Noofelements);
-	if (Noofelements<= 0) 
-	{
-		printf("The array elements cannot be stored\n"); 
-		exit(1); 
+void main() {
+	int max_p = INT_MIN;
+	int max_s = INT_MIN;
+	int i,a[100];
+	for(i=0;i<100;i++) {
+	    a[i] = rand()%1000;
 	}
-
-	array = (int *) malloc(sizeof(int) * Noofelements); 
-
-	srand(MAXIMUM); 
-	for (i = 0; i<Noofelements; i++) 
-		array[i] = rand(); 
-	if (Noofelements == 1)
-	 {
-		printf("The Largest Number In The Array is %d", array[0]);
-		exit(1); 
-	 }
-
-	cur_max = 0;
-        omp_set_num_threads(8);
-	#pragma omp parallel for 
-	for (i = 0; i<Noofelements; i = i + 1) 
-	{
-		if (array[i] >cur_max) 
-		#pragma omp critical 
-		if (array[i] >cur_max) 
-			cur_max = array[i]; 
+	printf("Check max through parallel\n");
+	#pragma omp parallel for num_threads(8)
+	for(i=0;i<100;i++) {
+	    if(a[i]>max_p) {
+	    	#pragma omp critical
+	    	max_p = a[i];
+	    }
 	}
-
-	current_value = array[0]; 
-	for (i = 1; i<Noofelements; i++) 
-		if (array[i] >current_value)
-	 		current_value = array[i]; 
-	printf("The Input Array Elements Are \n"); 
-	for (i = 0; i<Noofelements; i++) 
-		printf("\t%d", array[i]); 
-	printf("\n");
-
-	if (current_value == cur_max)
-		printf("\nThe Max Value Is Same From Serial And Parallel OpenMP Directive\n"); 
-	else 
-	{
-		printf("\nThe Max Value Is Not Same In Serial And Parallel OpenMP Directive\n"); 
-		exit(1);
-
-	printf("\n"); 
-	free(array); 
-	printf("\nThe Largest Number In The Given Array Is %d\n", cur_max); 
+	printf("Check max through serial\n");
+	for(i=0;i<100;i++) {
+	    if(a[i]>max_s)
+	        max_s = a[i];
+	}
+	printf("Parallel max %d serial max %d\n",max_p,max_s);
 }
-
-
-
-
